8c2db3a528ed411d0e391e7c8084b3c3081ba9ca
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-11 22:58:19 +0530

    Initial Commit

---
+++ ZenArcade/ZenArcadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenArcadeApp.swift
+//  ZenArcade
+//
+//  Created by sabiq on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ZenArcadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
